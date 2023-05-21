4272174f19983992a506ffec672f8bdf4aac0c9d
Brian Campbell <86080337+campbeb3@users.noreply.github.com>
2023-05-20 18:07:39 -0700

    Add files via upload

--- public_html/server.js
+++ public_html/server.js
@@ -1,12 +1,11 @@
+// REF: https://github.com/bpk68/api-server-starter/tree/master
+
 // load up the express framework and body-parser helper
 const express = require('express');
 const bodyParser = require('body-parser');
 
 // create an instance of express to serve our end points
 const app = express();
-
-// REF: https://github.com/bpk68/api-server-starter/tree/master
-
 const fs = require('fs');
 
 // configure express instance with some body-parser settings
@@ -14,10 +13,10 @@ const fs = require('fs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// handle our various routes
+// ROUTE HANDLER //
 const routes = require('./routes/routes.js')(app, fs);
 
-// launch our server on port 54544.
+// LAUNCH ON PORT 54544 //
 const server = app.listen(54544, () => {
   console.log('listening on port %s...', server.address().port);
 });
